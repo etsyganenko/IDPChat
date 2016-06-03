5a099f035937c0e038a372a796c45bd378878a72
Yevgen <yevgen.tsyganenko@idapgroup.com>
2016-06-03 18:37:44 +0300

    QuickBlox SDK added

---
+++ IdapChat/IdapChat/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IdapChat
+//
+//  Created by Artem on 6/3/16.
+//  Copyright © 2016 IDAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IdapChat/Pods/Target Support Files/Pods-IdapChat/Pods-IdapChat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IdapChat : NSObject
+@end
+@implementation PodsDummy_Pods_IdapChat
+@end
